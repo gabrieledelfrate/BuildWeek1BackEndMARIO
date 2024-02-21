1f156c2222a8f92159652120e9e718e03235c7c6
Alessandro Basile <alessandrobasile1992@gmail.com>
2024-02-21 17:13:43 +0100

    add user already registered control

--- Registrazione.aspx.cs
+++ Registrazione.aspx.cs
@@ -19,27 +19,34 @@ namespace MARIO
             string connectionString = ConfigurationManager.ConnectionStrings["Collegamento"].ConnectionString;
             using (SqlConnection con = new SqlConnection(connectionString))
             {
-                string query = "INSERT INTO Cliente (email, password) VALUES (@Email, @Password)";
-                SqlCommand cmd = new SqlCommand(query, con);
-
-                SqlParameter paramEmail = new SqlParameter("@Email", email);
-                SqlParameter paramPassword = new SqlParameter("@Password", password);
-                cmd.Parameters.Add(paramEmail);
-                cmd.Parameters.Add(paramPassword);
+                string checkQuery = "SELECT COUNT(*) FROM Cliente WHERE email = @Email";
+                SqlCommand checkCmd = new SqlCommand(checkQuery, con);
+                checkCmd.Parameters.AddWithValue("@Email", email);
 
                 try
                 {
                     con.Open();
-                    cmd.ExecuteNonQuery();
+                    int emailCount = (int)checkCmd.ExecuteScalar();
+                    if (emailCount > 0)
+                    {
+                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Questo indirizzo email è già stato registrato. Si prega di utilizzare un altro indirizzo email." + "');", true);
+                        return;
+                    }
+                    else
+                    {
+                        string insertQuery = "INSERT INTO Cliente (email, password) VALUES (@Email, @Password)";
+                        SqlCommand insertCmd = new SqlCommand(insertQuery, con);
+                        insertCmd.Parameters.AddWithValue("@Email", email);
+                        insertCmd.Parameters.AddWithValue("@Password", password);
+                        insertCmd.ExecuteNonQuery();
+                    }
                 }
                 catch (Exception ex)
                 {
                     Response.Write("Errore durante l'inserimento dei dati: " + ex.Message);
                 }
             }
-
             Response.Redirect("Login.aspx");
         }
-
     }
 }
